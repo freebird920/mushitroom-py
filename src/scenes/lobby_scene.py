af479dc264dacdb65dec9a777cefe7040f74e60e
퇴역군인 <freebird_han@naver.com>
2025-12-01 13:38:39 +0900

    update

--- src/scenes/lobby_scene.py
+++ src/scenes/lobby_scene.py
@@ -4,13 +4,17 @@ from PIL.ImageDraw import ImageDraw
 from classes.render_coordinate import RenderCoordinate
 from classes.render_size import RenderSize
 from classes.scene_base import BaseScene
+from components.render_button import RenderButton
+from components.render_image import RenderImage
 from components.render_text import RenderText
+from components.render_ui_component import RenderUiComponent
+from managers.input_manager import InputState
 from managers.sound_manager import SoundManager
 from managers.sq_manager import SqService
 from managers.ui_component_manager import UiComponentManager
 from schemas.user_schema import GameState, User
 from settings.mushitroom_config import DISPLAY_WIDTH
-from settings.mushitroom_enums import FontStyle
+from settings.mushitroom_enums import FontStyle, InputActions
 
 
 class LobbySceneArgs(TypedDict):
@@ -56,8 +60,6 @@ class LobbyScene(BaseScene):
         self._game_state = game_state
         print(f"[System] 로비 입장 완료: {self._user_id}")
 
-    def draw(self, draw_tool: ImageDraw):
-        super().draw(draw_tool)
         user_id_text = RenderText(
             coordinate=RenderCoordinate(DISPLAY_WIDTH // 2, 10),
             color="black",
@@ -66,7 +68,46 @@ class LobbyScene(BaseScene):
             font_size=12,
             font_style=FontStyle.COOKIE_BOLD,
         )
-        user_id_text.draw(draw_tool)
+        user_id_text_component = RenderUiComponent(
+            is_selectable=False,
+            on_activate=None,
+            render_object=user_id_text,
+        )
+        self._ui_component_manager.add_component(user_id_text_component)
+
+        dance_button = RenderImage(
+            coordinate=RenderCoordinate(60, 200),
+            size=RenderSize(320 // 4, 100 // 4),
+            src="./src/assets/images/btn_adopt.png",
+        )
+        dance_button_component = RenderUiComponent(
+            is_selectable=True,
+            on_activate=None,
+            render_object=dance_button,
+        )
+        self._ui_component_manager.add_component(dance_button_component)
+        dance_button = RenderImage(
+            coordinate=RenderCoordinate(140, 200),
+            size=RenderSize(320 // 4, 100 // 4),
+            src="./src/assets/images/btn_dance.png",
+        )
+        dance_button_component = RenderUiComponent(
+            is_selectable=True,
+            on_activate=None,
+            render_object=dance_button,
+        )
+        self._ui_component_manager.add_component(dance_button_component)
+
+    def handle_input(self, input_state: InputState):
+        if InputActions.DOWN in input_state.just_pressed_actions:
+            print("눌림")
+        return super().handle_input(input_state)
+
+    def draw(self, draw_tool: ImageDraw):
+        super().draw(draw_tool)
+        self._ui_component_manager.draw(draw_tool)
+
+        # user_id_text.draw(draw_tool)
 
     def on_exit(self):
         super().on_exit()
